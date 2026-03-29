8c96606501e33429febf24dfcf1f8e438b2e4a6e
Vladyslav_Hrytsak <vlad.123jj@gmail.com>
2026-03-29 12:54:33 +0300

    fix listings stats

--- src/controllers/publicListing.controller.ts
+++ src/controllers/publicListing.controller.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
 import { IListingQuery } from "../interfaces/listingQuery.interface";
-import { listingViewRepository } from "../repositories/listingView.repository";
-import { listingViewService } from "../services/listingView.service";
 import { publicListingService } from "../services/publicListing.service";
 
 class PublicListingController {
@@ -21,8 +19,7 @@ class PublicListingController {
   public async getById(req: Request, res: Response, next: NextFunction) {
     try {
       const result = await publicListingService.getById(req.params.id);
-      await listingViewService.addView(req.params.id);
-      await listingViewRepository.createView(req.params.id);
+
 
       res.json(result);
     } catch (e) {
